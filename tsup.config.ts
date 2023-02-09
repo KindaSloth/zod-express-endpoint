a1ffe528ea3f36cbe082817e5293e897e606248b
Guilherme775 <guilhermereis28122002@gmail.com>
2023-02-09 20:07:35 -0300

    feat: add changeset and tree shaking

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'tsup';
 
+const env = process.env.NODE_ENV;
+
 export default defineConfig({
-    entry: ['src/index.ts'],
+    entryPoints: ['src/index.ts'],
     splitting: false,
-    sourcemap: true,
     clean: true,
     dts: true,
-    format: ['cjs']
+    format: ['cjs', 'esm'],
+    minify: env === 'production',
+    bundle: env === 'production',
+    skipNodeModulesBundle: true,
+    outDir: 'dist',
+    entry: ['src/*.ts', 'src/**/*.ts']
 });
